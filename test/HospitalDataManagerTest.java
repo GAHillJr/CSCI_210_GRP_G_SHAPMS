b7d85cd681b6bd8217894da7e908d73f32d3fe25
Gbralta <144059947+Gbralta@users.noreply.github.com>
2025-12-19 07:25:49 -0800

    Add unit tests for HospitalDataManager to validate patient and doctor management functionalities

---
+++ test/HospitalDataManagerTest.java
@@ -0,0 +1,108 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for HospitalDataManager.
+ */
+
+class HospitalDataManagerTest {
+
+    @Test
+    void addPatient() {
+    }
+
+    @Test
+    void searchPatientById() {
+    }
+
+    @Test
+    void searchPatientByName() {
+    }
+
+    @Test
+    void getAllPatientsSorted() {
+    }
+
+    @Test
+    void addDoctor() {
+    }
+
+    @Test
+    void searchDoctorById() {
+    }
+
+    @Test
+    void searchDoctorByName() {
+    }
+
+    @Test
+    void searchDoctorsBySpecialty() {
+    }
+
+    @Test
+    void getAllDoctorsSorted() {
+    }
+
+    @Test
+    void linkAppointmentToPatient() {
+    }
+
+    @Test
+    void getAppointmentsForPatient() {
+    }
+
+    @Test
+    void sortAppointmentsByDate() {
+    }
+
+    @Test
+    void sortAppointmentsByDoctor() {
+    }
+
+    @Test
+    void sortAppointmentsByPatient() {
+    }
+
+    @Test
+    void getMostConsultedDoctors() {
+    }
+
+    @Test
+    void getBusiestDoctor() {
+    }
+
+    @Test
+    void getDoctorConsultationCount() {
+    }
+
+    @Test
+    void getAllSpecialties() {
+    }
+
+    @Test
+    void addDepartment() {
+    }
+
+    @Test
+    void getAllDepartments() {
+    }
+
+    @Test
+    void hasSpecialty() {
+    }
+
+    @Test
+    void getTotalPatients() {
+    }
+
+    @Test
+    void getTotalDoctors() {
+    }
+
+    @Test
+    void getTotalAppointments() {
+    }
+
+    @Test
+    void displayStatistics() {
+    }
+}
